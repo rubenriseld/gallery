b6bfcf2fec5d3c3dc6b4520223723000c677c12b
rubenriseld <rubenriseld@outlook.com>
2023-08-12 16:38:47 +0200

    design adjustments and mobile menu burger menu for mobile css adjustments for Header

--- Gallery.UI/gallery/src/components/Header.jsx
+++ Gallery.UI/gallery/src/components/Header.jsx
@@ -1,16 +1,69 @@
 import { NavLink, Route } from 'react-router-dom/cjs/react-router-dom.min';
+import { useState, useEffect } from "react";
+import "./header.css";
+
 
 export default function Header() {
 
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
+    const [shadow, setShadow] = useState(false);
+    
+    const toggleBoxShadow = () => {
+        window.addEventListener('scroll', () => {
+            // currentScroll är användarens position i Y-led, från toppen av dokumentet 
+            const currentScroll = window.scrollY;
+            if (currentScroll > 10) {
+                setShadow(true);
+                return;
+            }
+            else{
+                setShadow(false);
+                return;
+            }
+        })
+    }
+    useEffect(() => {
+        const handleScrollEvent = () => {
+            toggleBoxShadow();
+        }
+        window.addEventListener('scroll', handleScrollEvent);
+        return () => {
+            window.addEventListener('scroll', handleScrollEvent);
+        };
+    }, []);
+
     return (
         <>
-            <header>
+        <header className={`${shadow ? "header-shadow" : ""} `}>
+            <div className='header-main'>
 
-                <h2 className="logo">Ruben Riseld</h2>
-                <nav className="menu">
-                    <NavLink to="/">Home</NavLink>
-                    <NavLink to="/testCollection">Collection</NavLink>
-                </nav>
+                    <NavLink to="/" onClick={()=> setShowMobileMenu(false)} className="logo">
+                        Ruben Riseld
+                    </NavLink>
+                <button
+                    className="burger-btn"
+                    onClick={() => setShowMobileMenu(!showMobileMenu)}>
+
+                    {!showMobileMenu? 
+                        <span className="material-symbols-outlined">
+                        menu
+                        </span>:
+                        <span className="material-symbols-outlined">
+                        close
+                        </span>
+                    }
+                </button>
+                    
+            </div>
+                <nav className={`${showMobileMenu ? "menu-show" : "menu-hidden"} `}>
+
+                    <NavLink to="/" onClick={()=> setShowMobileMenu(false)}>
+                        Home
+                    </NavLink>
+                    <NavLink to="/testCollection" onClick={()=> setShowMobileMenu(false)}>
+                        test collection
+                    </NavLink>
+                        </nav>
             </header>
         </>
     )
