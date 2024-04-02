49d4cac3d014fdf5684c62e73f89fff7d37fbce6
rubenriseld <rubenriseld@outlook.com>
2024-04-02 10:33:02 +0200

    Renamed Id property of ImageCollectionDTO

--- Gallery.API/DTOs/ImageCollectionDTO.cs
+++ Gallery.API/DTOs/ImageCollectionDTO.cs
@@ -7,14 +7,14 @@ public class CreateImageCollectionDTO
 }
 public class ReadImageCollectionDTO
 {
-    public required string Id { get; set; }
+    public required string ImageCollectionId { get; set; }
     public required string Name { get; set; }
     public string? Description { get; set; }
     public List<ReadImageDTO> Images { get; set; } = [];
 }
 public class UpdateImageCollectionDTO
 {
-    public required string Id { get; set; }
+    public required string ImageCollectionId { get; set; }
     public required string Name { get; set; }
     public string? Description { get; set; }
 }
