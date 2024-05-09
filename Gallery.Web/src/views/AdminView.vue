277df878bcc7e2d1225213c3baaeae547a569a52
rubenriseld <rubenriseld@outlook.com>
2024-05-09 18:25:28 +0200

    Add loading state to AdminView.vue

--- Gallery.Web/src/views/AdminView.vue
+++ Gallery.Web/src/views/AdminView.vue
@@ -9,14 +9,17 @@ import Manager from '@/components/Manager.vue'
 const collections = ref<ImageCollection[]>([])
 const images = ref<Image[]>([])
 const tags = ref<Tag[]>([])
+const isLoading = ref(true)
 
 onMounted(async () => {
     await refreshData()
 })
 async function refreshData() {
+    isLoading.value = true
     await getTags()
     await getImages()
     await getCollections()
+    isLoading.value = false
 }
 async function getTags() {
     tags.value = (await api.get('tags')).data as Tag[]
@@ -32,16 +35,16 @@ async function getCollections() {
 <template>
     <div>
         <TabMenu>
-            <template #Collections v-if="collections.length">
+            <template #Collections v-if="!isLoading">
                 <Manager :key="'collections'" :collections="collections" :objectType="'collections'"
                     :refresh="refreshData" :create="requests.createCollection" :update="requests.updateCollection"
                     :delete="requests.deleteCollection" />
             </template>
-            <template #Tags v-if="tags.length">
+            <template #Tags v-if="!isLoading">
                 <Manager :key="'tags'" :tags="tags" :objectType="'tags'" :refresh="refreshData"
                     :create="requests.createTag" :update="requests.updateTag" :delete="requests.deleteTag" />
             </template>
-            <template #Images v-if="images.length">
+            <template #Images v-if="!isLoading">
                 <Manager :key="'images'" :images="images" :tags="tags" :collections="collections" :objectType="'images'"
                     :refresh="refreshData" :create="requests.createImage" :update="requests.updateImage"
                     :delete="requests.deleteImage" />
