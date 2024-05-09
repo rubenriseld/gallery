faf388a2b7f4f414673993eb535525d83e36b2a9
rubenriseld <rubenriseld@outlook.com>
2024-05-09 18:26:50 +0200

    Updated styling in TabMenu.vue

--- Gallery.Web/src/components/TabMenu.vue
+++ Gallery.Web/src/components/TabMenu.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted} from 'vue'
+import { ref, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 
 const tabs = ref<string[]>(['Collections', 'Tags', 'Images'])
@@ -14,23 +14,23 @@ const setActiveTab = (index: number) => {
 }
 // Restore active tab from URL query parameter on component mount
 onMounted(() => {
-  const tabParam = router.currentRoute.value.query.tab
-  if (tabParam && !Array.isArray(tabParam) && !isNaN(Number(tabParam))) {
-    const index = parseInt(tabParam as string)
-    if (index >= 0 && index < tabs.value.length) {
-      setActiveTab(index)
+    const tabParam = router.currentRoute.value.query.tab
+    if (tabParam && !Array.isArray(tabParam) && !isNaN(Number(tabParam))) {
+        const index = parseInt(tabParam as string)
+        if (index >= 0 && index < tabs.value.length) {
+            setActiveTab(index)
+        }
     }
-  }
 })
 
 // Expose methods and variables
 const expose = { setActiveTab, tabs, currentTab };
 </script>
 <template>
-    <div>
-        <div class="tabs">
+    <div class="tab-wrapper">
+        <div class="tab-menu">
             <button v-for="(tab, index) in tabs" :key="index" @click="setActiveTab(index)"
-                :class="{ active: activeTab === index }">{{ tab }}</button>
+                :class="{ active: activeTab === index }" class="tab-button">{{ tab }}</button>
         </div>
         <div class="tab-content">
             <slot :name="currentTab"></slot>
@@ -38,23 +38,55 @@ const expose = { setActiveTab, tabs, currentTab };
     </div>
 </template>
 <style scoped>
-.tabs {
+.tab-wrapper {
+    width: 100%;
     display: flex;
+    flex-direction: column
 }
 
-.tabs button {
-    cursor: pointer;
-    padding: 10px 15px;
-    border: none;
-    background-color: #f0f0f0;
-    margin-right: 5px;
-}
-
-.tabs button.active {
-    background-color: #ccc;
+.tab-menu {
+    display: flex;
+    width: 100%;
+    margin: 2rem 2rem 1.5rem 2rem;
+    justify-items: center;
 }
 
 .tab-content {
-    margin-top: 10px;
+    display: flex;
+}
+
+.tab-button {
+    text-transform: uppercase;
+    margin-right: 1.5rem;
+    font-size: 1.2rem;
+    cursor: pointer;
+    border: none;
+    font-family: inherit;
+    padding-bottom: 0.5rem;
+}
+
+.tab-button.active,
+.tab-button.active:hover,
+.tab-button:hover {
+    background: linear-gradient(var(--primary-color), var(--primary-color)) no-repeat;
+    background-size: 100% 4px;
+    background-position: 0 100%;
+}
+
+.tab-button:hover {
+    background: linear-gradient(var(--secondary-color), var(--secondary-color)) no-repeat;
+    background-size: 100% 2px;
+    background-position: 0 95%;
+}
+
+@media (max-width: 768px) {
+    .tab-menu {
+        margin: 1rem 1rem 0.5rem 1rem;
+    }
+
+    .tab-button {
+        font-size: 1rem;
+        margin-right: 1rem;
+    }
 }
 </style>
