16903dd5b8942f21e01c6380bfc5bae5b0c85353
rubenriseld <rubenriseld@outlook.com>
2024-05-09 18:26:12 +0200

    Updateed Menu.vue to dynamically generate menu items based on image collections

--- Gallery.Web/src/components/Menu.vue
+++ Gallery.Web/src/components/Menu.vue
@@ -4,30 +4,41 @@ import { onMounted, onUnmounted, ref, watch } from 'vue'
 import { useRouter } from "vue-router"
 import IconMenu from './icons/IconMenu.vue'
 import IconClose from './icons/IconClose.vue'
+import type { ImageCollection } from '@/assets/types'
 
 const props = defineProps({
     isAuthenticated: Boolean
 })
 
+const collections = ref<ImageCollection[]>([])
 const isAuthenticated = ref<boolean>(props.isAuthenticated)
+const isMenuOpen = ref(false)
+const isLoading = ref(true)
 const router = useRouter()
 
-const menuItems = [
-    { name: 'Home', path: '/' },
-    { name: 'Collection', path: '/collection' },
-    { name: 'Admin', path: '/admin' },
-]
-
-const isMenuOpen = ref(false)
-
 onMounted(async () => {
+    isLoading.value = true
+    collections.value = (await api.get('imageCollections')).data as ImageCollection[]
     isAuthenticated.value = await authenticate()
+    updateMenuItems()
+    isLoading.value = false
 })
 watch([isAuthenticated], async () => {
-    console.log("auth menu")
     isAuthenticated.value = await authenticate()
-    console.log(isAuthenticated.value)
 })
+
+const menuItems = ref<Array<{ name: string; path: string }>>([
+    { name: 'Home', path: '/' },
+    { name: 'Admin', path: '/admin' }]
+)
+
+function updateMenuItems() {
+   collections.value.map(collection => 
+   menuItems.value.push({
+        name: collection.name,
+        path: `/collection/${collection.imageCollectionId}`
+    }))
+}
 
 async function logout() {
     await api.post('auth/logout')
@@ -46,14 +57,14 @@ async function authenticate(): Promise<boolean> {
 }
 </script>
 
-<template>
+<template v-if="!isLoading">
     <div class="menu-item-wrapper">
         <RouterLink to="/" class="logo">Ruben Riseld</RouterLink>
     </div>
     <div class="menu-container">
-        <div class="menu-icon" @click="isMenuOpen = !isMenuOpen">
-            <IconMenu v-if="!isMenuOpen" />
-            <IconClose v-else="isMenuOpen" />
+        <div  @click="isMenuOpen = !isMenuOpen">
+            <IconMenu class="menu-icon" v-if="!isMenuOpen" />
+            <IconClose class="menu-icon" v-else="isMenuOpen" />
         </div>
         <nav :class="{ 'show-menu': isMenuOpen }">
             <div class="menu-items">
@@ -65,7 +76,7 @@ async function authenticate(): Promise<boolean> {
                 </template>
             </div>
             <div v-if="isAuthenticated" class="menu-item-wrapper logout-wrapper">
-                <button @click="logout" class="logout-button">Log out</button>
+                <RouterLink class="menu-item logout-button" to="/" @click="logout" buttonText="">Log out</RouterLink>
             </div>
         </nav>
     </div>
@@ -78,7 +89,7 @@ async function authenticate(): Promise<boolean> {
 
 .menu-icon {
     color: #121212;
-    margin: 2rem;
+    padding: 2rem;
     cursor: pointer;
     display: flex;
     align-items: center;
@@ -89,12 +100,12 @@ async function authenticate(): Promise<boolean> {
 nav {
     position: fixed;
     top: 0;
-    right: 0;
+    right: calc((100vw - var(--max-width)) / 2);
     width: 100%;
     height: 100%;
     padding-top: 5rem;
     box-sizing: border-box;
-    background-color: #ffffff;
+    background-color: var(--lightest-color);
     z-index: 1;
     display: none;
 }
@@ -106,28 +117,30 @@ nav {
 }
 
 .menu-item-wrapper {
-    padding: 2rem;
-    height: 5rem;
+    padding: 2rem 2rem 1.5rem 2rem;
     box-sizing: border-box;
     display: flex;
     justify-content: flex-end;
 }
 
 .menu-item,
-.logo,
-.logout-button {
-    color: #121212;
+.logo
+{
     text-decoration: none;
-    font-weight: bold;
     text-transform: uppercase;
-    font-size: 1.2rem;
+    padding-bottom: 0.5rem;
+    font-size: 1.5rem;
+    letter-spacing: 2px;
     transition: background-color 0.3s;
+    color: var(--darker-color);
     font-family: inherit;
 }
 
-.menu-item-wrapper:hover {
-    background-color: #f0f0f0;
-    text-decoration: underline;
+.menu-item:hover,
+.logo:hover {
+    background: linear-gradient(var(--secondary-color), var(--secondary-color)) no-repeat;
+    background-size: 100% 4px;
+    background-position: 0 95%;
 }
 
 .show-menu {
@@ -135,18 +148,38 @@ nav {
     flex-direction: column;
     justify-content: space-between;
     width: 20rem;
-    box-shadow: -2px 0 5px rgba(0, 0, 0, 0.1);
+    box-shadow: -5px 0 5px rgba(0, 0, 0, 0.1);
+    background: linear-gradient(var(--secondary-color), var(--secondary-color)) no-repeat;
+    background-size: 4px 100cap;
+    background-position: 100% 50%;
+    padding-right:1rem;
+    overflow: visible;
+    background-color: var(--lightest-color);
 }
 
 @media (max-width: 768px) {
     .show-menu {
         width: 100%;
         padding: 0;
+        background:none;
+        background-color: var(--lightest-color);
     }
-
-    .show-menu>.menu-item-wrapper {
+    .menu-items {
+       padding-top:5rem;
+    }
+    .menu-icon{
+        padding:1rem;
+        width: 2rem;
+        height: 2rem;
+    }
+    .menu-item-wrapper {
+        padding:1rem;
         display: flex;
-        justify-content: center;
+    }
+}
+@media (max-width: 1920px) {
+    nav {
+        right: 0;
     }
 }
 </style>
