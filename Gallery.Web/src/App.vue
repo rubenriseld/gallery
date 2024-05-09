1c0612f9129edb40dfa950160647b593d200df4f
rubenriseld <rubenriseld@outlook.com>
2024-05-09 18:27:56 +0200

    Updated App.vue structure and styles

--- Gallery.Web/src/App.vue
+++ Gallery.Web/src/App.vue
@@ -7,63 +7,33 @@ import Menu from './components/Menu.vue'
     <header>
         <Menu/>
     </header>
-    <main>
 
-        <body>
+        <main>
             <RouterView class="view"></RouterView>
-        </body>
-    </main>
+        </main>
 </template>
 <style scoped>
-* {
-    margin: 0;
-    padding: 0;
-    font-family: 'Courier New', Courier, monospace;
-}
 
-#app {
-    max-width: 1280px;
-    margin: auto;
-}
-
-body {
-    margin-top: 5rem;
-    background-color: rgb(219, 219, 219);
-    font-family: 'Courier New', Courier, monospace;
-}
-
-button {
-    background-color: transparent;
-    border: none;
-    font-family: inherit !important;
-}
-
-.view {
-    padding: 2rem 0 2rem 0;
-    display: flex;
-}
-
-@media (hover: hover) {
-    a:hover {
-        /* background-color: hsla(160, 100%, 37%, 0.2); */
-    }
-}
-
-@media (min-width: 1024px) {
-    #app {}
-}
 
 header {
     position: fixed;
     top: 0;
-    left: 0;
     width: 100%;
+    max-width: var(--max-width);
     height: 5rem;
-    background-color: #ffffff;
-    z-index: 1000;
+    z-index: 1;
     display: flex;
     justify-content: space-between;
-    /* Add this line */
-    align-items: center
+    background-color: var(--lightest-color);
+    align-items: center;
+    overflow:hidden;
 }
+
+main {
+    margin-top: 5rem;
+    font-family: inherit;
+    background-color: inherit;
+    overflow: hidden;
+}
+
 </style>
