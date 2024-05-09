cd452acd486da6fd703719c27d89b7d1680eaf98
rubenriseld <rubenriseld@outlook.com>
2024-05-09 18:29:41 +0200

    Added logic and styles for IndexView.vue component

--- Gallery.Web/src/views/IndexView.vue
+++ Gallery.Web/src/views/IndexView.vue
@@ -1,5 +1,64 @@
+<script setup lang="ts">
+import api from '@/api';
+import type { ImageCollection } from '@/assets/types';
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+
+const isLoading = ref(true);
+const collectionId = ref<string>('');
+const collections = ref<ImageCollection[]>([]);
+const route = useRoute()
+
+onMounted(async () => {
+    const id = route.params.collectionId as string
+    collectionId.value = id
+    await getCollections()
+    console.log(collections.value)
+    isLoading.value = false
+})
+
+async function getCollections() {
+    collections.value = (await api.get("imageCollections")).data as ImageCollection[]
+}
+
+</script>
 <template>
-    <div>
-        index
+    <div class="collection-wrapper" v-for="(collection, index) in collections" :key="index">
+        <RouterLink class="collection-link" :to="`collection/${collection.imageCollectionId}`">{{ collection.name }}
+        </RouterLink>
     </div>
 </template>
+<style scoped>
+.collection-wrapper {
+    height: 15rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--mid-color);
+    margin: 1rem 2rem 1rem 2rem;
+}
+
+.collection-link {
+    height: 100%;
+    display: flex;
+    margin: 0 1rem 0 1rem;
+    justify-content: center;
+    align-items: center;
+    font-size: 3rem;
+    text-transform: uppercase;
+    color: var(--darker-color);
+    text-decoration: none;
+    transition: all 0.2s ease-in;
+}
+
+.collection-link:hover {
+    letter-spacing: 4px;
+    font-weight: 800;
+}
+
+@media (max-width: 768px) {
+    .collection-wrapper {
+        margin: 1rem 1rem 1rem 1rem;
+    }
+}
+</style>
