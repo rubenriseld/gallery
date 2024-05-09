db85c20b32ebfd24c95a192b98936d68f0787243
rubenriseld <rubenriseld@outlook.com>
2024-05-09 18:23:09 +0200

    Added PillCheckbox component for tag selection in forms

---
+++ Gallery.Web/src/components/PillCheckbox.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps(['tag', 'tagIndex', 'formData']);
+
+</script>
+<template>
+    <label :class="{ active: formData.tagIds.includes(tag.tagId) }" class="pill">
+        {{ tag.name }}
+        <input type="checkbox" :value="tag.tagId" v-model="formData.tagIds"
+               :id="'tagCheckbox_' + tagIndex" class="checkbox-hidden" />
+    </label>
+</template>
+
+<style scoped>
+.checkbox-hidden {
+    display: none;
+}
+.pill {
+    padding: 0.5rem;
+    margin:0.5rem;
+    background-color: var(--light-color);
+    cursor: pointer;
+    user-select: none;
+}
+.pill.active {
+    background-color: var(--secondary-color);
+}
+.pill:hover {
+    background-color: var(--secondary-color-hover);
+}
+.pill.active:hover {
+    background-color: var(--primary-color-hover);
+}
+</style>
