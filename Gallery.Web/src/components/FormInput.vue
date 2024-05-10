69dc6f5ad8261b47f8750a4bda3068fcae43faaf
rubenriseld <rubenriseld@outlook.com>
2024-05-10 11:46:57 +0200

    Add new form components for TagWrapper, FormButtons, FormInput, and CollectionDropdownSelect

---
+++ Gallery.Web/src/components/FormInput.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+const props = defineProps({
+    label: String,
+    modelValue: null,
+    placeholder: String
+})
+
+const emit = defineEmits(['update:modelValue'])
+function emitData(event: Event) {
+    const value = (event.target as HTMLSelectElement)?.value;
+    console.log(value)
+    emit('update:modelValue', value)
+}
+</script>
+
+<template>
+    <div class="form-input-wrapper">
+        <label :for="label">{{ label }}</label>
+        <input :placeholder="placeholder"
+            :value="modelValue"
+            @input="emitData($event)">
+    </div>
+</template>
+
+<style scoped>
+.form-input-wrapper {
+    width: 100%;
+    margin-bottom: 1rem;
+    display: flex;
+    flex-direction: column;
+}
+
+label {
+    margin-bottom: 0.5rem;
+}
+
+input {
+    height: 2rem;
+    padding: 0.25rem;
+    font-size: 1rem;
+    font-family: inherit;
+}
+</style>
---
+++ Gallery.Web/src/components/TagWrapper.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import PillCheckbox from './PillCheckbox.vue'
+
+const props = defineProps({
+    tags: Array,
+    formData: Object
+})
+
+</script>
+
+<template>
+    <div class="form-input-wrapper">
+        <label for="tagIds">Tags</label>
+        <div class="tag-wrapper">
+            <PillCheckbox v-for="(tag, tagIndex) in tags" :key="tagIndex" :tag="tag" :tagIndex="tagIndex"
+                :formData="formData" />
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+.form-input-wrapper {
+    width: 100%;
+    margin-bottom: 1rem;
+    display: flex;
+    flex-direction: column;
+}
+
+label {
+    margin-bottom: 0.5rem;
+}
+
+.tag-wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0 -0.5rem 0 -0.5rem;
+}
+</style>
