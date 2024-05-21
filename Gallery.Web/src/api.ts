c33c9eb30d58ea7a5a2025bf9adebf0ef47f3010
rubenriseld <rubenriseld@outlook.com>
2024-05-21 07:30:15 +0200

    Exclude "/api/*" from navigation fallback, set baseURL for axios to "/api"

--- Gallery.Web/src/api.ts
+++ Gallery.Web/src/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useRouter } from 'vue-router'
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: "/api/",
     headers: {
         'Content-Type': 'application/json',
     },
